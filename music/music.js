8e29fec5ee62aa9eee405fb7956cfb726121f44d
Your Name <you@example.com>
2019-12-04 21:36:01 +0800

    Site updated: 2019-12-04 21:36:00

--- music/music.js
+++ music/music.js
@@ -6,8 +6,8 @@ const ap = new APlayer({
       {
         name: "请回答1988(悲伤姻缘)",
         artist: '晚霞',
-        url: 'http://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=75731359&type=convert_url&response=res',
-        cover: 'http://p4.music.126.net/0GuMykiQNW_r6uQ_6wv7XQ==/109951163949085548.jpg?param=200y200',
+        url: 'https://antiserver.kuwo.cn/anti.s?format=mp3|aac&rid=75731359&type=convert_url&response=res',
+        cover: 'https://p4.music.126.net/0GuMykiQNW_r6uQ_6wv7XQ==/109951163949085548.jpg?param=200y200',
       },
     ]
 });
